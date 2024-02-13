24910457cd45205e961ad8339fed61b75cfb9b7b
kenzie0624 <112727924+kenzie0624@users.noreply.github.com>
2024-02-13 11:30:20 -0600

    Updated Visualization

--- DataVisualization.py
+++ DataVisualization.py
@@ -1,13 +1,68 @@
-import numpy as np
-from matplotlib import pyplot as plt
+# Load libraries
+# remove unused imports in final project if code is turned in
 from pandas import read_csv
+from pandas.plotting import scatter_matrix
+from matplotlib import pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+import sklearn as sk
+from sklearn.decomposition import PCA
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import PolynomialFeatures
 from scipy import stats
+import sklearn.metrics as met
+
+
 # Keicimen = 0
 # Besni = 1
 
 # ----BEFORE CLEANING----
 names = ['area', 'major', 'minor', 'ecc', 'convex', 'extent', 'perimeter', 'class']
 dataset = read_csv('Raisin_Dataset_CSV.csv', header=0, names=names)
+# summarize the dataset
+print(dataset.describe())
+print(dataset.sample(20))
+# count the missing values
+print(dataset.isnull().sum())
+
+# locate the column with the 99999 values
+num_missing = (dataset == 99999).sum()
+print(num_missing)
+# find and print the indices
+for col in dataset:
+    if num_missing[col] != 0:
+        indices = dataset[dataset[col] == 99999].index
+        print(col + ":" + str(list(indices)))
+
+# locate the column with the 0 values
+num_missing = (dataset == 0).sum()
+print(num_missing)
+# find and print the indices
+# we can ignore the 0 values in class because 0 is a valid value
+for col in dataset:
+    if num_missing[col] != 0:
+        indices = dataset[dataset[col] == 0].index
+        print(col + ":" + str(list(indices)))
+
+
+# ----DATA PLOTS----
+
+# Box Plot
+dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.show()
 
 # Density Curve
 dataset.plot(kind='density', subplots=True, layout=(3, 3), sharex=False)
@@ -67,7 +122,7 @@ ax.set_yticklabels(names)
 plt.show()
 
 
-# Clean the Data
+# ----Clean the Data----
 threshold_z = 3
 outlier_indices = []
 for col in dataset:
@@ -75,11 +130,18 @@ for col in dataset:
         z = np.abs(stats.zscore(dataset[col]))
         outlier_indices_col = np.where(z > threshold_z)[0]
         outlier_indices.extend(outlier_indices_col)
+print(outlier_indices)
 dataset = dataset.drop(outlier_indices)
+print(dataset.describe())
 
 
+# ----DATA PLOTS AFTER CLEANING----
 
-# ----AFTER CLEANING----
+# Box Plot
+dataset.plot(kind='box', subplots=True, layout=(3,3), sharex=False, sharey=False)
+plt.show()
+
+
 
 # Density Plot
 dataset.plot(kind='density', subplots=True, layout=(3, 3), sharex=False)
